e6f8c6e00bde6c826d3148228891a2c32d370170
Abdelrahman <dopagraming@gmail.com>
2025-08-07 13:09:26 +0300

    ds

--- index.js
+++ index.js
@@ -21,16 +21,15 @@ dotenv.config();
 
 const app = express();
 
-app.use(helmet());
-
 app.use(cors({
     origin: process.env.FRONTEND_URL || 'http://localhost:5173',
     credentials: true
 }));
 
-// app.options('*', cors());
-
 app.use(compression());
+
+app.use(helmet());
+
 
 
 app.use(
