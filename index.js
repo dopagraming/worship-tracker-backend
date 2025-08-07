e05bc65b0eb11f2f668201deb3f794a4788a9cb4
Abdelrahman <dopagraming@gmail.com>
2025-08-07 19:34:48 +0300

    update the cors ur;

--- index.js
+++ index.js
@@ -21,7 +21,7 @@ dotenv.config();
 const app = express();
 
 app.use(cors({
-    origin: process.env.FRONTEND_URL || 'http://localhost:5173',
+    origin: process.env.FRONTEND_URL || 'https://ibadat-frontend.vercel.app',
     credentials: true
 }));
 
